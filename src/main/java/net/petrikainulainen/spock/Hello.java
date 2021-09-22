6f90f7ba21de045232bb8c00dc20c582ab66aee0
ramreddykiran <ramreddykiran@gmail.com>
2021-09-22 15:42:32 +0530

    first commit

---
+++ src/main/java/net/petrikainulainen/spock/Hello.java
@@ -0,0 +1,8 @@
+package net.petrikainulainen.spock;
+
+public final class Hello {
+
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
---
+++ src/test/groovy/net/petrikainulainen/spock/HelloSpec.groovy
@@ -0,0 +1,17 @@
+package net.petrikainulainen.spock
+
+import spock.lang.Specification
+
+
+class HelloSpec extends Specification {
+
+    def hello = new Hello();
+
+    def sayHello() {
+        given: "A person's name is given as a method parameter."
+        def greeting = hello.sayHello("Petri");
+
+        expect: "Should say hello to the person whose name is given as a method parameter"
+        greeting == "Hello Petri";
+    }
+}
